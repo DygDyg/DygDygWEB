c4931e17b53c695451d36211a6966fe795aeaaac
DygDyg <33259562+DygDyg@users.noreply.github.com>
2021-05-22 14:55:51 +0300

    fsdf

---
+++ unity/VK_API/script.js
@@ -0,0 +1,17 @@
+
+    //var gameInstance = UnityLoader.instatiate("gameContainer", "Build/webgl.json");
+    myGameInstance.SendMessage('URL_PLANE', 'loadscreen', 'https://sun9-6.userapi.com/impg/E0ddoxmLsorc3Xd2qELJ1l_q5NwLM2l0RTjHzg/ELrcNpmHWOA.jpg?size=1919x1079&quality=96&sign=9ecee202aec8313e6f99cc15f4a50b10&type=album');
+    //var unityInstance = UnityLoader.instantiate("unityContainer", "Build/build.json", {onProgress: UnityProgress});
+
+
+
+
+    myGameInstance.SendMessage("Text", "GetTextMess", "test123");
+
+
+
+      
+
+        //console.log("Проверка связи");
+        //alert("Тест стороннего файла");
+
