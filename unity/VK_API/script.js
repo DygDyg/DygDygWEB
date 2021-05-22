8e5494c8ba246b8c5e316d3bb7655a0b845ae187
DygDyg <33259562+DygDyg@users.noreply.github.com>
2021-05-22 15:52:22 +0300

    Update script.js

--- unity/VK_API/script.js
+++ unity/VK_API/script.js
@@ -4,6 +4,5 @@
     myGameInstance.SendMessage('URL_PLANE', 'loadscreen', 'https://sun9-6.userapi.com/impg/E0ddoxmLsorc3Xd2qELJ1l_q5NwLM2l0RTjHzg/ELrcNpmHWOA.jpg?size=1919x1079&quality=96&sign=9ecee202aec8313e6f99cc15f4a50b10&type=album');
     myGameInstance.SendMessage("Text", "GetTextMess", "test123");
 
-    alert(window.location.href);
-    alert(document.URL);
+ 
 
