f674e4c8ba02a8ed66e7f8c6019666bbca6b4ca0
DygDyg <33259562+DygDyg@users.noreply.github.com>
2021-05-25 09:53:30 +0300

    d

--- unity/VK_API/Build/VK_API.framework.js
+++ unity/VK_API/Build/VK_API.framework.js
@@ -1291,7 +1291,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2604832;
+STATICTOP = STATIC_BASE + 2605424;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3313,7 +3313,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2604832;
+var STATIC_BUMP = 2605424;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
