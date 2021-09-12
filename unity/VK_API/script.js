3286b461ab2598c512575f4b59f9d08db2d8cc88
DygDyg <33259562+DygDyg@users.noreply.github.com>
2021-09-13 02:35:54 +0300

    a

--- unity/VK_API/script.js
+++ unity/VK_API/script.js
@@ -1,56 +1,46 @@
 function getUrlParameter(sParam) {
-  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-      sURLVariables = sPageURL.split('&'),
-      sParameterName,
-      i;
+	var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+		sURLVariables = sPageURL.split('&'),
+		sParameterName,
+		i
 
-  for (i = 0; i < sURLVariables.length; i++) {
-      sParameterName = sURLVariables[i].split('=');
+	for (i = 0; i < sURLVariables.length; i++) {
+		sParameterName = sURLVariables[i].split('=')
 
-      if (sParameterName[0] === sParam) {
-          return sParameterName[1] === undefined ? true : sParameterName[1];
-      }
-  }
-};
+		if (sParameterName[0] === sParam) {
+			return sParameterName[1] === undefined ? true : sParameterName[1]
+		}
+	}
+}
 
-
-
-var req = $("#VK_URL_JSON").text();
+var req = $('#VK_URL_JSON').text()
 //alert(req);
 
-
-if(getUrlParameter('access_token'))
-{
-  req = req + "&access_token=" + getUrlParameter('access_token');
-  console.log("a");
-}
-else
-{
-  req = req + "&access_token=" + "0d6bdfbd0d6bdfbd0d6bdfbd390d1c30e000d6b0d6bdfbd6dccaa2bb030f8333b4fd3fe";
-  console.log("b");
+if (getUrlParameter('access_token')) {
+	req = req + '&access_token=' + getUrlParameter('access_token')
+	//console.log('a')
+} else {
+	req = req + '&access_token=' + '0d6bdfbd0d6bdfbd0d6bdfbd390d1c30e000d6b0d6bdfbd6dccaa2bb030f8333b4fd3fe'
+	//console.log('b')
 }
 
 $.ajax({
-  url: req,            
-  dataType : "jsonp",               
-  success: function (data, textStatus) {
-    
+	url: req,
+	dataType: 'jsonp',
+	success: function (data, textStatus) {
+    console.log(data.response)
+		for (const item in data.response.items) {
+			const element = data.response.items[item]
+			if (element.photo_1280) {
+				myGameInstance.SendMessage('Main', 'Json_base_URL_add', element.photo_1280)
+				//myGameInstance.SendMessage('Main', 'Json_base_text_add', data.response.items[i].text);
+			}
+		}
 
-    for (let i=0; i< data.response.count; i++)
-    {
-     if(data.response.items[i].photo_1280)
-     {
-        myGameInstance.SendMessage('Main', 'Json_base_URL_add', data.response.items[i].photo_1280);
-        //myGameInstance.SendMessage('Main', 'Json_base_text_add', data.response.items[i].text);
-     }
-    }
-    myGameInstance.SendMessage('Main', 'Start_Load_Texture');
-  }
-});
+		myGameInstance.SendMessage('Main', 'Start_Load_Texture')
+	}
+})
 
 //console.log(window.location);
-
-
-
 
 //console.log(getUrlParameter('access_token'));
