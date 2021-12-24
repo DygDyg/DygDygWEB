5f8ff91fb42dab2b7517476bd5e519c4cf4d14c3
DygDyg <dygdyg@inbox.ru>
2021-12-24 20:55:20 +0300

    Update youtube_hide.js

--- youtube_hide.js
+++ youtube_hide.js
@@ -1,24 +1,6 @@
-var dyg_num = 0, dyg_numes = 0, dyg_ver = "v6";
+var dyg_num = 0, dyg_numes = 0, dyg_ver = "v7";
 
 console.log("скрываю видео! "+dyg_ver)
-
-
-function add_style()
-{
-   if(hide_style==null){
-       hide_style = GM_addStyle(`#hide_video
-       {
-       display: none !important;
-       }`)
-    //    console.log(2,hide_style)
-   }else{
-       hide_style.remove()
-       hide_style = null
-    //    console.log(1,hide_style)
-   }
-}
-
-
 
 function non_video()
 {
@@ -28,36 +10,26 @@ function non_video()
     for(let i = 0; i<frames.length;i++) {
         if(frames[i].querySelector('div#progress')!=null && frames[i].style.display != 'none' && frames[i].id !="items" && frames[i].id !="hide_video")
         {
-            //if(parseInt(frames[i].querySelector('div#progress').style.width)>=prochent)
-            //{
                 frames[i].style.display = 'none'
-                //frames[i].parentElement.removeChild(frames[i])
-                //frames[i].id = 'hide_video'
                 dyg_numes++
                 dyg_num++
-            //}
         }
     }
     if(document.querySelector('span#country-code')){
         document.querySelector('span#country-code').textContent = "Скрыто видео: "+parseInt(dyg_num)+" "+dyg_ver;
-        // console.log(dyg_num)
     }
 }
-
-//var menuCmdprochent = GM_registerMenuCommand('Пропуск выключен', () => {
-//    let str=prompt("Запрос на ввод данных", 0);
-//})
-
 
 document.querySelector("body").onscroll = function(){
     non_video()
 }
 
 
-document.addEventListener('keydown', function(event) {
-    if(event.repeat==false){
-        if(event.key=="\\"&&event.ctrlKey==true&&event.shiftKey==false&&event.altKey==false){
-            // add_style()
-        }
-    }
-})
+//document.addEventListener('keydown', function(event) {
+//    if(event.repeat==false){
+//        if(event.key=="\\"&&event.ctrlKey==true&&event.shiftKey==false&&event.altKey==false){
+//           
+//        }
+//    }
+//})
+//
