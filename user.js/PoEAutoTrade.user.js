4b942207be7b86145dbba79551de91e007f6771f
DygDyg <DygDyg@inbox.ru>
2023-08-24 14:05:36 +0300

    modified:   user.js/PoEAutoTrade.user.js

--- user.js/PoEAutoTrade.user.js
+++ user.js/PoEAutoTrade.user.js
@@ -68,6 +68,11 @@ function tims() {
         document.querySelector('.btn.btn-xs.btn-default.direct-btn').click()
         document.querySelector('.btn.btn-xs.btn-default.direct-btn').parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove()
     }
+    if (document.querySelectorAll('.btn.btn-xs.btn-default.dropdown-toggle.dropdown-toggle-split').length > 0) {
+        // document.querySelector('.btn.btn-xs.btn-default.direct-btn').remove()
+        // document.querySelector('.btn.btn-xs.btn-default.direct-btn').click()
+        document.querySelector('.btn.btn-xs.btn-default.dropdown-toggle.dropdown-toggle-split').parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove()
+    }
 }
 
 if (document.TradeTimers == null) {
