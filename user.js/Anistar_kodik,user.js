4129be982a104ae1c5efa6afcaedeaa35e87d474
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-04-22 14:51:27 +0300

    /

--- user.js/Anistar_kodik,user.js
+++ user.js/Anistar_kodik,user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Anistar kodik
+// @name         Anistar kodik pleer
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
