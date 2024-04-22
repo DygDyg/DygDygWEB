28ce8bf24619fff44fa21e9e9e2eb5a0a4a1c88c
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-04-22 14:49:49 +0300

    /

--- user.js/Anistar_kodik,user.js
+++ user.js/Anistar_kodik,user.js
@@ -16,44 +16,43 @@
 
 spavn()
 
-function spavn(title1)
-{
-		if(!document.body.querySelector('[itemprop="name"]')) return
-		_switch = true
-		title1 = document.body.querySelector('[itemprop="name"]').textContent.split('/')
+function spavn(title1) {
+    if (!document.body.querySelector('[itemprop="name"]')) return
+    _switch = true
+    title1 = document.body.querySelector('[itemprop="name"]').textContent.split('/')
 
-		document.body.querySelector('[itemprop="name"]').addEventListener("click", (event) => {
-			_switch = !_switch
+    document.body.querySelector('[itemprop="name"]').addEventListener("click", (event) => {
+        _switch = !_switch
 
-			title = _switch ? title1[1] : title1[0]
+        title = _switch ? title1[1] : title1[0]
 
-			link = encodeURI(`//dygdyg.github.io/DygDygWEB/kodik.htm?title=${title}`)
-			console.log(link)
-			console.log("fdfdf",_switch)
-			kodik_url.href = link
-			kodik_url.title = title
-			document.body.querySelector('#kodik').src = link
-		});
+        link = encodeURI(`//dygdyg.github.io/DygDygWEB/kodik.htm?title=${title}`)
+        console.log(link)
+        console.log("fdfdf", _switch)
+        kodik_url.href = link
+        kodik_url.title = title
+        document.body.querySelector('#kodik').src = link
+    });
 
 
-		kodik_url = document.createElement("a");
-		kodik = document.createElement("div");
-		// title1 = title1 || document.body.querySelector('[itemprop="name"]').textContent.split('/')[1]
-		// if(_switch) title = title1[1] || title1[0]
+    kodik_url = document.createElement("a");
+    kodik = document.createElement("div");
+    // title1 = title1 || document.body.querySelector('[itemprop="name"]').textContent.split('/')[1]
+    // if(_switch) title = title1[1] || title1[0]
 
-		title = title1[1]
-		console.log("fdfdf",_switch)
-		console.log(title)
+    title = title1[1]
+    console.log("fdfdf", _switch)
+    console.log(title)
 
-		// var link = `//kodik.info/find-player?title=${title}&camrip=false`
-		link = encodeURI(`//dygdyg.github.io/DygDygWEB/kodik.htm?title=${title}`)
-		console.log(link)
-		kodik.innerHTML = `<iframe id="kodik" src="${link}" frameborder="0" allow="fullscreen"></iframe>`;
-		kodik_url.textContent = "Открыть на сервере"
-		kodik_url.href = link
-		kodik_url.title = title
-		document.body.querySelector('.vide_be').prepend(kodik);
-		document.body.querySelector('.vide_be').prepend(kodik_url);
+    // var link = `//kodik.info/find-player?title=${title}&camrip=false`
+    link = encodeURI(`//dygdyg.github.io/DygDygWEB/kodik.htm?title=${title}`)
+    console.log(link)
+    kodik.innerHTML = `<iframe id="kodik" src="${link}" frameborder="0" allow="fullscreen"></iframe>`;
+    kodik_url.textContent = "Открыть на сервере"
+    kodik_url.href = link
+    kodik_url.title = title
+    document.body.querySelector('.vide_be').prepend(kodik);
+    document.body.querySelector('.vide_be').prepend(kodik_url);
 
 };
 
