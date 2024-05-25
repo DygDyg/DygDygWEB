d5a479acfde00d9abc81935f8f97ac0eb321ce88
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-05-25 07:07:57 +0300

    //

--- NewTab/base.js
+++ NewTab/base.js
@@ -488,7 +488,7 @@ function add_card(url) {
 	let images_tmp = []
 	let scr_url = getUrlParameter('s-shot')?`//mini.s-shot.ru/1680x1050/JPEG/320/Z100/?${url}`:`//api.screenshotmachine.com/?key=e51b85&dimension=480x270&url=${url}`
 	// images_tmp[num] = images[num]?images[num]:`https://api.screenshotmachine.com/?key=e51b85&dimension=1024x768&url=${url}`
-
+	images[num] = images[num].includes(".s-shot.ru/?")?"":images[num] //Удаляет ссылку на s-shot.ru
 	images_tmp[num] = images[num]?images[num]:scr_url
 
 	// images_tmp[num] = images[num]?images[num]:`https://mini.s-shot.ru/1680x1050/JPEG/320/Z100/?${url}`
