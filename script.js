2ee00fb4be4c430e8e4d7ba9d48eef6a54c43b27
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-07-27 06:14:41 +0300

    //

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const querystring = require('querystring');
 const url = 'https://discord.com/api/webhooks/627316395075567616/f51PMQc4CiUR0XBFEHoYeh25nYpceK5UI0qPHpQDUqqgfe4q3u3Z4Tv1whhKZRIdPBJ0';
 const data = {
     username: 'DygDyg',
-    content: 'hello world'
+    avatar_url: 'https://www.freepngimg.com/thumb/facebook/62681-flat-icons-face-computer-design-avatar-icon.png',
+    content: 'hello world',
+
 };
 
 axios.post(url, querystring.stringify(data), {
