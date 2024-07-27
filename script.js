3721d1b1a9d76ed5b06cd4760220914574ebeca9
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-07-27 06:34:58 +0300

    //

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+const fs = require('fs');
 const axios = require('axios');
 const querystring = require('querystring');
+const filePath = 'id.txt';
+let id = readIdFromFile();
+function readIdFromFile() {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        return parseInt(data, 10);
+    } catch (err) {
+        console.error('Error reading id from file:', err);
+        return 0; // Если файл не найден или ошибка чтения, вернем 0
+    }
+}
+
+function writeIdToFile(id) {
+    try {
+        fs.writeFileSync(filePath, id.toString(), 'utf8');
+        console.log('Successfully saved id:', id);
+    } catch (err) {
+        console.error('Error writing id to file:', err);
+    }
+}
 
 const url = 'https://discord.com/api/webhooks/627316395075567616/f51PMQc4CiUR0XBFEHoYeh25nYpceK5UI0qPHpQDUqqgfe4q3u3Z4Tv1whhKZRIdPBJ0';
 const data = {
@@ -10,7 +31,7 @@ const data = {
 #  [TV] Моя подруга-олениха Нокотан 
 [Вышла 20 дней назад. 07.07.2024]
 
-> **Серии:** 4/?  
+> **Серии:** 4/? ${id}  
 > **Длительность:** 23 минуты
 > **Студия:** [Студии:  Wit Studio](<https://track-anime.github.io/?anime_studios=%D0%A1%D1%82%D1%83%D0%B4%D0%B8%D0%B8%3A%20%20Wit%20Studio>) 
 > **Год выхода:** 2024
@@ -29,14 +50,20 @@ const data = {
 
 };
 
+
+
 axios.post(url, querystring.stringify(data), {
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
     }
 })
-.then(response => {
-    console.log('Success:', response.data);
-})
-.catch(error => {
-    console.error('Error:', error);
-});
+    .then(response => {
+        console.log('Success:', response.data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+
+id += 1;
+writeIdToFile(id);
