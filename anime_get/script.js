44c79cd1a43d2d573e3a22d2434b27cbb672635d
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-07-28 20:07:48 +0300

    //

--- anime_get/script.js
+++ anime_get/script.js
@@ -2,70 +2,115 @@ const fs = require('fs');
 const axios = require('axios');
 const querystring = require('querystring');
 const filePath = 'id.txt';
-let id = readIdFromFile();
+var id = readIdFromFile();
 
 function readIdFromFile() {
-    try {
-      const data = fs.readFileSync(filePath, 'utf8');
-      return parseInt(data, 10);
-    } catch (err) {
-      console.error('Error reading id from file:', err);
-      return 0; // Если файл не найден или ошибка чтения, вернем 0
-    }
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    return parseInt(data, 10);
+  } catch (err) {
+    console.error('Error reading id from file:', err);
+    return 0; // Если файл не найден или ошибка чтения, вернем 0
   }
-  
-  // Функция для записи параметра id в файл
-  function writeIdToFile(id) {
-    try {
-      fs.writeFileSync(filePath, id.toString(), 'utf8');
-      console.log('Successfully saved id:', id);
-    } catch (err) {
-      console.error('Error writing id to file:', err);
-    }
-  }
+}
 
-const url = 'https://discord.com/api/webhooks/627316395075567616/f51PMQc4CiUR0XBFEHoYeh25nYpceK5UI0qPHpQDUqqgfe4q3u3Z4Tv1whhKZRIdPBJ0';
-const data = {
+// Функция для записи параметра id в файл
+function writeIdToFile(id) {
+  try {
+    fs.writeFileSync(filePath, id.toString(), 'utf8');
+  } catch (err) {
+    console.error('Error writing id to file:', err);
+  }
+}
+
+const url = 'https://discord.com/api/webhooks/1266606126389133353/DCwGYmgpxtgGwioPqxgvr-ztnj-R9QP2o_xq8LurFSi1DJdLoCrnPoI7ExLJ9kGLyXq1';
+var id_start
+
+
+(async () => {
+  let id = readIdFromFile();
+  console.log('Loaded id:', id);
+
+  // Пример GET-запроса
+  try {
+    let dat = []
+
+    const response = await axios.get('https://kodikapi.com/list?limit=10&with_material_data=true&camrip=false&token=45c53578f11ecfb74e31267b634cc6a8');
+    const dataArray = response.data; // Преобразование JSON в массив
+    // console.log('Fetched data:', dataArray);
+
+    // Пример обновления id
+    // console.log('Updated id:', id);
+    id_start = dataArray.results[0].id;
+    dataArray.results.forEach(e => {
+      // console.log(e.type, e.title)
+      if (e.type.includes("anime")) {
+        dat.push(e)
+      }
+    });
+    // console.log(dat)
+    wh(dat)
+
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+})();
+
+
+function wh(es) {
+  if (es.length <= 0) {
+    writeIdToFile(id_start);
+    return
+  }
+  if (es[0].id == id) return;
+
+  let e = es[0]
+  es.shift()
+  console.log(e.title)
+  const data = {
     username: 'Track Anime By ДугДуг',
     avatar_url: 'https://track-anime.github.io/favicon.png',
     content: `
-~~                                                                                                                                                                                          ~~
-#  [TV] Моя подруга-олениха Нокотан 
-[Вышла 20 дней назад. 07.07.2024]
-    ${id} 
-> **Серии:** 4/?  
-> **Длительность:** 23 минуты
-> **Студия:** [Студии:  Wit Studio](<https://track-anime.github.io/?anime_studios=%D0%A1%D1%82%D1%83%D0%B4%D0%B8%D0%B8%3A%20%20Wit%20Studio>) 
-> **Год выхода:** 2024
-> **Жанры:**  
-> **Статус:** Релиз  
-> **Возрастной рейтинг:** [pg_13](<https://track-anime.github.io/?rating_mpaa=pg_13>)
-> 
-> **Рейтинг shikimori:** 7.76/10 (2478 проголосовавших)
+  ~~                                                                                                                                                                                          ~~
+# [${e?.material_data?.anime_kind ? e.material_data.anime_kind.toUpperCase() : "?"}] ${e?.title}
+  > **Перевод:** ${e.translation.title}
+  > **Серия:** ${e.episodes_count}
+  > **Длительность:** ${e.material_data?.duration}
+  > **Студия:** [Студия:  ${e.material_data?.anime_studios?.[0]}](<https://track-anime.github.io/?anime_studios=${e.material_data?.anime_studios?.[0]}>) //
+  > **Год выхода:** ${e.year}
+  > **Жанры:**  //
+  > **Статус:** Релиз  //
+  > **Возрастной рейтинг:** ${e.material_data?.rating_mpaa}
+  > 
+  > **Рейтинг shikimori:** ${e.material_data?.rating_mpaa}/10 (${e.material_data.shikimori_votes} проголосовавших)
+  
+  [Открыть на Track Anime By ДугДуг](<https://track-anime.github.io/?shikimori_id=${e.shikimori_id}>)
+  [Открыть на shikimori](<https://shikimori.one/animes/${e.shikimori_id}>)
+  
+  [Обложка](${e.material_data?.anime_poster_url})
+      
+  `,
 
-[Открыть на Track Anime By ДугДуг](<https://track-anime.github.io/?shikimori_id=58426>)
-[Открыть на shikimori](<https://shikimori.one/animes/58426>)
-
-[Обложка](https://shikimori.one/system/animes/original/58426.jpg?1718266672)
-    
-`,
-
-};
-
-
-
-axios.post(url, querystring.stringify(data), {
-    headers: {
+  };
+  setTimeout(() => {
+    axios.post(url, querystring.stringify(data), {
+      headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
-    }
-})
-    .then(response => {
-        console.log('Success:', response.data);
+      }
     })
-    .catch(error => {
+      .then(response => {
+        console.log('Success:', response.data);
+      })
+      .catch(error => {
         console.error('Error:', error);
-    });
+      });
+    wh(es)
+  }, 1000);
+}
 
 
-id += 1;
-writeIdToFile(id);
+
+
+// id += 1;
+// writeIdToFile(id);
