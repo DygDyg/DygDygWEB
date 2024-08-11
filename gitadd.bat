c4e44ae5a5415e356f082ac5837c85447cd35a59
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-08-11 22:26:01 +0300

    11-08-2024_22-26-01-53

--- gitadd.bat
+++ gitadd.bat
@@ -1,4 +1,10 @@
+@echo off
+
+for /f "tokens=1-4 delims=. " %%a in ('echo %date%') do set dt=%%a-%%b-%%c
+for /f "tokens=1-4 delims=:., " %%a in ('echo %time%') do set tm=%%a-%%b-%%c-%%d
+set datetime=%dt%_%tm%
+echo %datetime%
 git add .
-git commit -m "Автоматический коммит"
+git commit -m "%datetime%"
 git push origin master
 pause
