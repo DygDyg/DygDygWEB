86d145c0e1f6f33aae82e4c9b715812c8d764a0d
DygDyg <33259562+DygDyg@users.noreply.github.com>
2024-08-13 03:59:10 +0300

    13-08-2024_3-59-09-84

--- gitadd.bat
+++ gitadd.bat
@@ -1,6 +1,5 @@
 @echo off
 scan.exe
-pause
 for /f "tokens=1-4 delims=. " %%a in ('echo %date%') do set dt=%%a-%%b-%%c
 for /f "tokens=1-4 delims=:., " %%a in ('echo %time%') do set tm=%%a-%%b-%%c-%%d
 set datetime=%dt%_%tm%
