f5a27db9cd901e9a60b26544f6b01f96cb281a30
DygDyg <dygdyg@inbox.ru>
2025-06-28 04:44:45 +0300

    Сб-28-06_4-44-45

--- user.js/Wowhead_rusifikator.user.js
+++ user.js/Wowhead_rusifikator.user.js
@@ -1,27 +1,37 @@
 // ==UserScript==
-    // @name         Wowhead русификатор
-    // @namespace    http://tampermonkey.net/
-    // @version      1.1
-    // @description  Переводит на русский описания предметов с вовхеда
-    // @author       ДугДуг
-    // @match        *://*/*
-	// @icon         https://www.google.com/s2/favicons?sz=64&domain=https://www.wowhead.com
-	// @updateURL    https://dygdyg.github.io/DygDygWEB/DygDygWEB/user.js/Wowhead_rusifikator.user.js
-	// @downloadURL  https://dygdyg.github.io/DygDygWEB/DygDygWEB/user.js/Wowhead_rusifikator.user.js
-    // @grant        none
-    // ==/UserScript==
+// @name         Wowhead русификатор
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Переводит на русский описания предметов с вовхеда
+// @author       ДугДуг
+// @match        *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=https://www.wowhead.com
+// @updateURL    https://dygdyg.github.io/DygDygWEB/DygDygWEB/user.js/Wowhead_rusifikator.user.js
+// @downloadURL  https://dygdyg.github.io/DygDygWEB/DygDygWEB/user.js/Wowhead_rusifikator.user.js
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
 
-    (function() {
-        // Проверяем наличие скрипта power.js
-        const hasPowerScript = Array.from(document.scripts).some(script =>
-            script.src === 'https://wow.zamimg.com/widgets/power.js'
-        );
-        // Если скрипт power.js найден, выполняем замену ссылок
-        if (hasPowerScript) {
-            const links = document.querySelectorAll('a[href*="wowhead.com"]');
-            links.forEach(link => {
-                link.href = link.href.replace('https://www.wowhead.com', 'https://ru.wowhead.com')
-                                    .replace('https://wowhead.com', 'https://ru.wowhead.com');
-            });
-        }
-    })();
+
+const hasPowerScript = Array.from(document.scripts).some(script =>
+    script.src === 'https://wow.zamimg.com/widgets/power.js'
+);
+// Если скрипт power.js найден, выполняем замену ссылок
+if (hasPowerScript) {
+    console.log("Перевожу на русский")
+    rusification()
+    setInterval(() => {
+        rusification()
+    }, 5000);
+}
+
+function rusification() {
+    const links = document.querySelectorAll('a[href*="wowhead.com"]');
+    links.forEach(link => {
+        link.href = link.href.replace(/https:\/\/(?!ru\.wowhead\.com)[^\/]*wowhead\.com/, 'https://ru.wowhead.com');
+        // .replace('https://wowhead.com', 'https://ru.wowhead.com');
+    });
+}
+
+
+
