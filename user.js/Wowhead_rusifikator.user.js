7f7eced07d609fa0654cd10cafee7d8d04b1553e
DygDyg <dygdyg@inbox.ru>
2025-06-30 00:12:19 +0300

    Пн-30-06_0-12-19

--- user.js/Wowhead_rusifikator.user.js
+++ user.js/Wowhead_rusifikator.user.js
@@ -1,37 +1,33 @@
 // ==UserScript==
 // @name         Wowhead русификатор
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Переводит на русский описания предметов с вовхеда
 // @author       ДугДуг
 // @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=https://www.wowhead.com
 // @updateURL    https://dygdyg.github.io/DygDygWEB/DygDygWEB/user.js/Wowhead_rusifikator.user.js
 // @downloadURL  https://dygdyg.github.io/DygDygWEB/DygDygWEB/user.js/Wowhead_rusifikator.user.js
-// @run-at       document-end
+// @run-at       document-idle
 // @grant        none
 // ==/UserScript==
 
 
-const hasPowerScript = Array.from(document.scripts).some(script =>
-    script.src === 'https://wow.zamimg.com/widgets/power.js'
-);
-// Если скрипт power.js найден, выполняем замену ссылок
-if (hasPowerScript) {
-    console.log("Перевожу на русский")
+rusification()
+setInterval(() => {
     rusification()
-    setInterval(() => {
-        rusification()
-    }, 5000);
-}
-
+}, 3000);
 function rusification() {
-    const links = document.querySelectorAll('a[href*="wowhead.com"]');
+    const links = document.querySelectorAll('a[href*="wowhead.com"]:not([href*="ru.wowhead.com"])');
+    if (links.length == 0) return
+    console.log("Переведено: ", links.length)
     links.forEach(link => {
         link.href = link.href.replace(/https:\/\/(?!ru\.wowhead\.com)[^\/]*wowhead\.com/, 'https://ru.wowhead.com');
         // .replace('https://wowhead.com', 'https://ru.wowhead.com');
     });
 }
+
+
 
 
 
