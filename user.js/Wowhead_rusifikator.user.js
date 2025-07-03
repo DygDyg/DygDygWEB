5728850b955375bf02e61711bd524c059fc261fe
DygDyg <dygdyg@inbox.ru>
2025-07-04 00:00:33 +0300

    Пт-04-07_0-00-32

--- user.js/Wowhead_rusifikator.user.js
+++ user.js/Wowhead_rusifikator.user.js
@@ -6,8 +6,8 @@
 // @author       ДугДуг
 // @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=https://www.wowhead.com
-// @updateURL    https://dygdyg.github.io/DygDygWEB/DygDygWEB/user.js/Wowhead_rusifikator.user.js
-// @downloadURL  https://dygdyg.github.io/DygDygWEB/DygDygWEB/user.js/Wowhead_rusifikator.user.js
+// @updateURL    https://dygdyg.github.io/DygDygWEB/user.js/Wowhead_rusifikator.user.js
+// @downloadURL  https://dygdyg.github.io/DygDygWEB/user.js/Wowhead_rusifikator.user.js
 // @run-at       document-idle
 // @grant        none
 // ==/UserScript==
@@ -15,15 +15,16 @@
 
 rusification()
 setInterval(() => {
-    rusification()
+   rusification()
 }, 3000);
-function rusification() {
-    const links = document.querySelectorAll('a[href*="wowhead.com"]:not([href*="ru.wowhead.com"])');
-    if (links.length == 0) return
-    console.log("Переведено: ", links.length)
+function rusification()
+{
+ const links = document.querySelectorAll('a[href*="wowhead.com"]:not([href*="ru.wowhead.com"])');
+if (links.length == 0) return
+console.log("Переведено: ", links.length)
     links.forEach(link => {
         link.href = link.href.replace(/https:\/\/(?!ru\.wowhead\.com)[^\/]*wowhead\.com/, 'https://ru.wowhead.com');
-        // .replace('https://wowhead.com', 'https://ru.wowhead.com');
+           // .replace('https://wowhead.com', 'https://ru.wowhead.com');
     });
 }
 
